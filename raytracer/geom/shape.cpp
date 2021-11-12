96d7c1d2752f82bf8091af2f6c4ae8be61b1b884
Mark Holum <mark.holum@ucalgary.ca>
2021-11-12 13:21:53 -0700

    The geometry routines now compile, and I reorganized the library to make it easier to work on....  I'm a little embarraed, I really don't know c++

---
+++ raytracer/geom/shape.cpp
@@ -0,0 +1,292 @@
+#include "shape.h"
+#include <math.h>
+
+namespace geom {
+
+
+	uv::uv(nicefp au, nicefp av) {
+
+		v = av;
+		u = au;
+	}
+	uv::uv(double au, double av) {
+
+		uv(nicefp(au), nicefp(av));
+	}
+
+
+
+	triangle::triangle(vec3 aposition, vec3 aside_a, vec3 aside_b, vec3 anormal, uv uv_a, uv uv_b, uv uv_c) {
+
+		position = aposition;
+		side_a = aside_a;
+		side_b = aside_b;
+		this->uv_a = uv_a;
+		this->uv_b = uv_b;
+		this->uv_c = uv_c;
+
+
+		normal = anormal;
+		area = normal.norm() / 2;
+
+	}
+
+		
+
+	void triangle::translate(const vec3 &by)  {
+
+		position = position + by;
+	}
+
+
+	void triangle::rotate(const mat3 &by)  { // don't rotate triangles unless to absolutely must 
+
+		position.rotate(by);
+		side_a.rotate(by);
+		side_b.rotate(by);
+
+		normal.rotate(by); // update normal!!! (could rotate, but this prevents errors?)
+
+	}
+
+	void triangle::scale(const vec3 &by)  {
+
+		position = position.mult(by);
+		side_a = side_a.mult(by);
+		side_b = side_b.mult(by);
+		normal = normal.mult(by).mult(by);
+		area = normal.norm() / 2;
+
+	}
+
+
+	bool triangle::calc_interception(ray& ray, nicefp within_d, interception& out)  {
+
+		// this uses the Moller-Trumbore algorithm
+		// note i'm not doing backface culling, since I'll need to intersect with backfaces for refraction
+		// algorithm was mostly copied from the original article, with some mods 
+
+		if (ray.direction.dot(normal) == 0) return false;
+
+		nicefp t, u, v;
+
+
+
+		vec3 edge1 = side_a;
+		vec3 edge2 = side_b;
+		vec3 origin_to_tri = ray.origin - position;
+
+		nicefp determinant = ray.direction.cross(edge2).dot(edge1);
+
+		if (determinant == 0) return false; // parallel to triangle
+
+		vec3 p = ray.direction.cross(edge2);
+
+		vec3 q = origin_to_tri.cross(edge1);
+
+		u = p.dot(origin_to_tri);
+
+		if (u < 0 || determinant < u) return false;
+
+		v = ray.direction.dot(q);
+
+		if (v < 0 || determinant < u + v) return false;
+
+
+		t = q.dot(edge2);
+		if (t < 0 ) return false; // missed triangle
+
+		out.distance = t;
+		out.position = position + ((edge1 * u) + (edge2 * v)) / determinant;
+		out.normal = get_normal(out.position);
+		out.atuv = getuv(out.position);
+		return true;
+	}
+
+	uv triangle::getuv(vec3 &intersect) { //using barycentric cooredinate
+		
+		if (area == 0) return uv_a; // cut out early for really small triangles
+
+		vec3 delta_p = intersect - position;
+
+		vec3 APBcross = side_a.cross(delta_p);
+		vec3 APCcross = side_b.cross(delta_p);
+
+		nicefp area2 = area*area;
+
+		nicefp beta = APBcross.dot(normal)/area2;
+		nicefp gamma = APCcross.dot(normal)/area2;
+		nicefp alpha = nicefp(1) - beta - gamma;
+
+		nicefp u = uv_a.u * alpha + uv_b.u * beta + uv_c.u * gamma;
+		nicefp v = uv_a.v * alpha + uv_b.v * beta + uv_c.v * gamma;
+
+		return uv(u, v);
+	}
+
+	vec3 triangle::get_normal(const vec3 &position)  {
+
+		return (normal * 2) / area; // area is half the norm of the normal
+	}
+	
+
+
+
+
+	sphere::sphere(vec3 acenter, vec3 orientationvec, vec3 orientation_equatorvec, nicefp amaxangle, nicefp aradius) { // maxangle in cos nicefps
+
+		center = acenter;
+		orientation = orientationvec;
+		orientation_equator = orientation_equatorvec;
+		maxangle = amaxangle;
+		radius = aradius;
+	}
+
+
+	vec3 sphere::get_normal(const vec3 &position)  {
+
+		return (center - position) / (radius * -1); // i hate hate hate that you cant declare const this!  
+
+	}
+
+	void sphere::translate(const vec3 &by)  {
+
+		center.translate(by);
+	}
+
+
+	void sphere::rotate(const mat3 &by)  { 
+
+		center.rotate(by);
+		orientation.rotate(by);
+		orientation_equator.rotate(by);
+	}
+
+	void sphere::scale(const vec3 &by)  { // note elipses are not supported!!!!  make sure all scales are diagonal
+
+		center = center.mult(by);
+		radius = radius * by.x;  // acchch this is wrong!! 
+	}
+
+	bool sphere::calc_interception(ray& ray, const nicefp within_d, interception& out) {
+
+		// uses the algorithm on page 76 of Shirley and Marschner
+		bool didintercept;
+
+		vec3 delta_o_c = ray.origin - center;
+		nicefp d_dot_d = ray.direction.dot(ray.direction);
+		nicefp d_dot_o_c = ray.direction.dot(delta_o_c);
+		
+		nicefp descriminant = (d_dot_o_c * d_dot_o_c) - (d_dot_d *(delta_o_c.dot(delta_o_c) - radius*radius));
+
+		if (descriminant < 0) return false;
+
+		nicefp sd = nicesqrt(descriminant);
+
+		nicefp distance_p = sd - d_dot_o_c; // note direction is always normalized!
+
+		nicefp distance_m = -sd - d_dot_o_c;
+
+
+		if (distance_m < distance_p && distance_m.value > 0 && distance_m < within_d) {
+				nicefp possible_distance = distance_m;
+				vec3 possible_position = delta_o_c + ray.direction * distance_m ; // i.e. orign + direction* distance - center
+				if (maxangle < (possible_position - center).dot(orientation)) {
+					out.distance = possible_distance;
+					out.position = possible_position;
+					didintercept = true;
+				} 
+			}
+		
+
+		if (distance_p.value > 0 && distance_p < within_d) {
+			nicefp possible_distance = distance_p;
+			vec3 possible_position = delta_o_c + ray.direction * distance_p; // i.e. orign + direction* distance - center
+
+			if (maxangle < (possible_position - center).dot(orientation)) {
+				out.distance = possible_distance;
+				out.position = possible_position;
+				didintercept = true;
+			}
+		}
+
+ 
+		if (didintercept) {
+
+			out.normal = get_normal(out.position);
+			out.atuv = getuv(out.position);
+			return true;
+		}
+
+		return false;
+		
+	}
+
+	uv sphere::getuv(vec3 &position)  {
+
+		vec3 delta = (center - position) / radius;
+		double odotd = orientation.dot(delta).value;
+
+		double v = acos(odotd)/ M_PI;
+		vec3 equat_delta = delta - orientation * odotd; // project delta onto plane;
+		double u = atan2(orientation_equator.cross(equat_delta).norm().value, orientation_equator.dot(equat_delta).value) / (2 * M_PI);
+
+		return uv(u, v); 
+
+
+	}
+
+
+
+	plane::plane(vec3 acenter, vec3 anormal, vec3 au_axis, vec3 av_axis) {
+
+		center = acenter;
+		normal = anormal / anormal.norm();; // should be normalized!
+		u_axis = au_axis / au_axis.norm();
+		v_axis = av_axis / au_axis.norm();
+	}
+
+	bool plane::calc_interception(ray& ray, nicefp within_d, interception& out)  {
+
+		nicefp denom =ray.direction.dot(normal);
+		if (denom == 0) return false; //parralel to plane
+
+		out.distance = (center - ray.origin).dot(normal) / denom;
+		out.position = ray.origin + ray.direction * out.distance;
+		out.normal = normal;
+
+		return true;
+
+	}
+
+
+	void plane::translate(const vec3 &by)  { // always translate after rotating
+
+		center.translate(by);
+
+	}
+	void plane::rotate(const mat3 &by)  { // always rotate before translation
+
+		normal.rotate(by);
+		u_axis.rotate(by);
+		v_axis.rotate(by);
+	}
+
+	void plane::scale(const vec3 &by) {}; // do nothing
+
+	uv plane::getuv(vec3 &position)  {
+
+		vec3 deltap = position - center;
+
+		nicefp u = deltap.dot(u_axis);
+		nicefp v = deltap.dot(v_axis);
+
+		return uv(u, v);
+	}
+
+	vec3 plane::get_normal(const vec3 &position)  {
+
+		return  normal;
+
+	}
+}
--- raytracer/geom/shape.h
+++ raytracer/geom/shape.h
@@ -1,3 +1,9 @@
+#include "nicefp.h"
+#include "vec3.h"
+#include "mat3.h"
+#include "ray.h"
+
+
 namespace geom {	
 
 	struct uv {
@@ -5,7 +11,10 @@ namespace geom {
 		nicefp u;
 		nicefp v;
 
+		uv() = default;
 		uv(nicefp au, nicefp av);
+		uv(double au, double av);
+
 
 	};
 
@@ -21,37 +30,33 @@ namespace geom {
 
 	struct shape { 
 
-		virtual bool calc_interception(const ray& theray, const nicefp within_d, interception& out);
-		virtual void translate(vec3 &by); // always translate after rotating
-		virtual void rotate(vec3 &by); // always rotate before translation
-		virtual void scale(double factor);
+		virtual bool calc_interception(ray& theray, const nicefp within_d, interception& out);
+		virtual void translate(const vec3 &by); // always translate after rotating
+		virtual void rotate(const mat3 &by); // always rotate before translation
+		virtual void scale(const vec3 &by);
 		virtual uv getuv(vec3 &position);
-		virtual vec3 get_normal(vec3 &position) = 0;
+		virtual vec3 get_normal(const vec3 &position);
 
 
 	};
 
 	struct triangle: shape {
 
-		vec3 point_a;
-		vec3 point_b;
-		vec3 point_c;
+		vec3 position;
+		vec3 side_a;
+		vec3 side_b;
 		uv uv_a, uv_b, uv_c;
-
 		vec3 normal;
-		nicefp area2;
 		nicefp area;
 
-		triangle(vec3 aposition, vec3 aside_a, vec3 aside_b, uv uv_a, uv uv_b, uv uv_c);
+		triangle(vec3 aposition, vec3 aside_a, vec3 aside_b, vec3 anormal, uv uv_a, uv uv_b, uv uv_c);
 
-		virtual void translate(const vec3 &by);
-		virtual void rotate(const mat3 &by); 
-		virtual void scale(double factor);
-
-
-		virtual bool calc_interception(const ray& theray, const nicefp within_d, interception& out); 
-
-		uv getuv(const vec3 &intersect);
+		virtual void translate(const vec3 &by) override;
+		virtual void rotate(const mat3 &by) override; 
+		virtual void scale(const vec3 &by) override;
+		virtual bool calc_interception(ray& theray, const nicefp within_d, interception& out) override; 
+		virtual uv getuv(vec3 &intersect) override;
+		virtual vec3 get_normal(const vec3 &position) override; 
 	};
 	
 
@@ -68,12 +73,12 @@ namespace geom {
 
 		sphere(const vec3 acenter, vec3 orientationvec, vec3 orientation_equatorvec, nicefp amaxangle, nicefp aradius); 
 
-		virtual vec3 get_normal(const vec3 &position); 
-		virtual void translate(const vec3 &by); 
-
-		virtual void rotate(const vec3 &by); 
-
-		virtual bool calc_interception(const ray& theray, const nicefp within_d, interception& out); 
+		virtual vec3 get_normal(const vec3 &position) override; 
+		virtual uv getuv(vec3 &position) override;
+		virtual void translate(const vec3 &by) override; 
+		virtual void rotate(const mat3 &by) override; 
+		virtual void scale(const vec3 &by) override;
+		virtual bool calc_interception(ray& theray, const nicefp within_d, interception& out) override;
 	};
 
 	struct plane: shape {
@@ -84,12 +89,12 @@ namespace geom {
 		vec3 v_axis; 
 
 		plane(vec3 acenter, vec3 anormal, vec3 au_axis, vec3 av_axis);
-		virtual bool calc_interception(const ray& theray, const nicefp &within_d, interception& out);
-		virtual void translate(const vec3 &by);
-		virtual void rotate(const vec3 &by);
-		virtual void scale(double factor);
-		virtual uv getuv(const vec3 &position);
-		virtual vec3 get_normal(const vec3 &position);
+		virtual bool calc_interception(ray& theray, const nicefp within_d, interception& out) override;
+		virtual void translate(const vec3 &by) override;
+		virtual void rotate(const mat3 &by) override;
+		virtual void scale(const vec3 &by) override;
+		virtual uv getuv(vec3 &position) override;
+		virtual vec3 get_normal(const vec3 &position) override;
 	};
 
 }
