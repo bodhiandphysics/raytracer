e84624f6b2361bcddbf698c463d84fac7da18b23
Mark Holum <mark.holum@ucalgary.ca>
2021-11-12 19:36:07 -0700

    added multithreading

--- raytracer/renderer.cpp
+++ raytracer/renderer.cpp
@@ -1,53 +1,48 @@
-#include <geom.h>
+#pragma once
+#include "world.h"
 #include <stdio.h>
 #include <stb_image_write.h>
-
-struct camera {
-
-
-	volatile vec3 position;
-	volatile vec3 direction;
-	volatile vec3 image_udirection;
-	volatile vec3 image_vdirection
-	volatile nicefp focal_length;
-	volatile nicefp u_size; // half sizes of image
-	volatile nicefp v_size;
-	volatile double* image;
-
-	volatile int height; // half height and length
-	volatile int length;
-
-	volatile int cutoff;
+#include <thread>
+#include <vector>
 
 
-	void render(int start, int stride, world::world theworld, int cutoff, double maxdistance) {
 
-		vec3 position = this.position;
-		vec3 direction = this.direction;
-		vec3 image_udirection = this.image_udirection
-		vec3 image_vdirection = this.image_vdirection
-		nicefp focal_length = this.focal_length
-		double u_scale = this.u_size / this.length;  // half sizes of image
-		double v_scale = this.v_size / this.height;
-		vec3* image = this.image;
-		int height = this.height; // half height and length
-		int length = this.length;
+void camera::rendertask(int start, int stride, world::world theworld, int cutoff, double maxdistance) {
 
-		vec3
-		for (int i = -length - 1 + start; i < length; i += stride) {
+	vec3 position = this.position;
+	vec3 direction = this.direction;
+	vec3 image_udirection = this.image_udirection
+	vec3 image_vdirection = this.image_vdirection
+	nicefp focal_length = this.focal_length
+	double u_scale = this.u_size / this.length;  // half sizes of image
+	double v_scale = this.v_size / this.height;
+	vec3* image = this.image;
+	int height = this.height; // half height and length
+	int length = this.length;
 
-			for (int j = -height - 1; j < height; j++) {
-				
-				// this could be optomized
-				vec3 pixel_loc = position + (direction * focal_length) + (image_udirection * i * u_scale) + (image_vdirection * j * v_scale);
-				geom::ray theray(pixel_loc, pixel_loc - position)
-				rt::rayoflight lightray(theray, nicefp(1), nicefp(0));
+	for (int i = -length - 1 + start; i < length; i += stride) {
 
-				image[i + length + 1][j + height + 1] = raytrace(lightray, theworld, cutoff, nicefp(maxdistance));
-			}
+		for (int j = -height - 1; j < height; j++) {
+			
+			// this could be optomized
+			vec3 pixel_loc = position + (direction * focal_length) + (image_udirection * i * u_scale) + (image_vdirection * j * v_scale);
+			geom::ray theray(pixel_loc, pixel_loc - position)
+			rt::rayoflight lightray(theray, nicefp(1), nicefp(0));
+
+			image[i + length + 1][j + height + 1] = raytrace(lightray, theworld, cutoff, nicefp(maxdistance));
 		}
 	}
-};
+}
+
+void camera::render(world::world &theworld, int cutoff, double maxdistance) { // this is dumb multithreading, but it should work on average
+	int numthreads = std::thread::hardware_concurrency();
+	std::vector<thread> threads;
+
+	for (int threadnum = 0; threadnum < numthreads; threadnum++)
+		threads.emplace_back(rendertask, threadnum, numthreads, theworld, cutoff, maxdistance);
+
+	for (int threadnum = 0; threadnum < numthreads; threadnum++) threads[i].join();
+}
 
 void create_png(const char* filename, double* pixeldata, int height, int width) {
 
