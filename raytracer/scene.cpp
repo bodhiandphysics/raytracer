4248aa6f0b64de8252bb5b11184a7f7e268241d6
Mark Holum <mark.holum@ucalgary.ca>
2021-11-16 22:56:08 -0700

    fixing

--- raytracer/scene.cpp
+++ raytracer/scene.cpp
@@ -33,7 +33,7 @@ world::world *create_scene() {
 	theball.translate(translationamount);
 	theworld->addsurfacegroup(theball);
 
-	//theworld->addlight(surf::light(vec3(1,1,1), vec3(0,0, 5), vec3(0,0,-1), nicefp(-1)));
+	theworld->addlight(surf::light(vec3(1,1,1), vec3(0,0, 5), vec3(0,0,-1), nicefp(-1)));
 
 	return theworld;  
 }
