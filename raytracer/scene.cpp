edb0ed10fb9975e8d689ea4acd71d4a7fc9161cf
Mark Holum <mark.holum@ucalgary.ca>
2021-11-17 16:11:19 -0700

    adding another light

--- raytracer/scene.cpp
+++ raytracer/scene.cpp
@@ -41,7 +41,7 @@ struct ballmaterial2 : phong {
 	   doesreflect = true;
 	}
 
-	virtual color materialcolor(const geom::uv &location) override {return color(0, 0 ,1);}
+	virtual color materialcolor(const geom::uv &location) override {return color(1, 1, ,1);}
  	virtual color ambient(const geom::uv &location) override {return color(1,1,1);}
   	virtual color diffuse(const geom::uv &location) override {return color(1,1,1);}
   	virtual color specular(const geom::uv &location) override {return color(1,1,1);}
@@ -74,6 +74,7 @@ world::world *create_scene() {
 	theworld->addsurfacegroup(anotherball);
 
 	theworld->addlight(surf::light(vec3(10000,10000,10000), vec3(0,0, 5), vec3(0,0,-1), nicefp(-1)));
+	theworld->addlight(surf::light(vec3(10000,10000,10000), vec3(0,0, -6), vec3(0,0,-1), nicefp(-1)));
 
 	return theworld;  
 }
